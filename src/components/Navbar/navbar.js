d426043b1fa04ca8a61575953cef682dc1a550c5
Gökhan Özdemir <goekhan.oezdemir90@googlemail.com>
2022-07-17 16:44:00 +0200

    stlye navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,21 +1,31 @@
+import EventIcon from '@mui/icons-material/Event';
+import HomeIcon from '@mui/icons-material/Home';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Box from '@mui/material/Box';
 import {Link} from 'react-router-dom';
 
-import {FooterWrapper} from './styled';
-
-export default function NavBar() {
+export default function Create() {
+	const navigationStyle = {background: '#35383F', height: '50px', opacity: '0.95'};
+	const iconStyle = {color: 'white'};
 	return (
-		<FooterWrapper>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-
-					<li>
-						<Link to="/events">Events</Link>
-					</li>
-				</ul>
-			</nav>
-		</FooterWrapper>
+		<Box sx={{width: 375}}>
+			<BottomNavigation showLabels style={navigationStyle}>
+				<Link to="/">
+					<BottomNavigationAction label="Home" icon={<HomeIcon />} style={iconStyle} />
+				</Link>
+				<Link to="events">
+					<BottomNavigationAction label="Events" icon={<EventIcon />} style={iconStyle} />
+				</Link>
+				<Link to="library">
+					<BottomNavigationAction
+						label="Library"
+						icon={<LibraryBooksIcon />}
+						style={iconStyle}
+					/>
+				</Link>
+			</BottomNavigation>
+		</Box>
 	);
 }

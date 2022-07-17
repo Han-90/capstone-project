f6c2873c57c14f957504c939ea9a631130fae2e8
Gökhan Özdemir <goekhan.oezdemir90@googlemail.com>
2022-07-17 16:58:41 +0200

    add notes to navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -5,11 +5,17 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import Box from '@mui/material/Box';
 import {Link} from 'react-router-dom';
+// Ich habe die Material UI Library genutzt. https://mui.com/material-ui/getting-started/installation/ unter folgendem Installationspfad zu finden.
+// Nach dem ich ich die Library mit dem Node Package Manager installiert habe, musste ich auch die Icons importieren um sie überhaupt nutzen zu können.
+// mit Hilfe von import {Link} verlinke ich auf meine Seiten über die Navigationsleiste. Hierzu muss ich in der App.js React Router importieren.
 
-export default function Create() {
+export default function CreateBottomNavigation() {
+	// ein Funktionsaufruf in dem ich meine Navigationsleiste erstelle und gleichzeitig auch exportiere, um sie als Komponente wieder verwenden zu können
 	const navigationStyle = {background: '#35383F', height: '50px', opacity: '0.95'};
+	// ich deklariere eine Konstante um meine Elemente zu stylen. In den Properties weise ich die Styles zu und überreiche sie im jeweiligen Tag.
 	const iconStyle = {color: 'white'};
 	return (
+		// nachdem ich meine Funktion aufgerufen habe und somit einen Input erstellt habe, muss ich auch einen Output gewährleisten, den ich innerhalb des return scopes ausgebe.
 		<Box sx={{width: 375}}>
 			<BottomNavigation showLabels style={navigationStyle}>
 				<Link to="/">

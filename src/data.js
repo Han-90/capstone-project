cc645bad466d87c290630a70fe7bc351bef4bcef
Gökhan Özdemir <98271167+Han-90@users.noreply.github.com>
2022-07-18 16:28:03 +0200

    style detailed page and cards (#23)

--- src/data.js
+++ src/data.js
@@ -6,36 +6,36 @@ import frankSinatra from './images/FrankSinatra.jpeg';
 import ludovicoEinaudi from './images/LudovicoEinaudi.jpeg';
 const inspirations = [
 	{
-		id: nanoid(),
+		id: '30773b84-e382-4665-b3af-624fb6ded94f',
 		cover: frankSinatra,
 		interpret: 'Frank Sinatra',
 		song: 'Fly me to the moon',
-		lyrics: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et',
+		lyrics: 'https://genius.com/Frank-sinatra-fly-me-to-the-moon-lyrics',
 		musicsheet: 'https://musescore.com/user/2538606/scores/4455576',
 	},
 	{
-		id: nanoid(),
+		id: '99967672-ffa2-4536-9c10-6041eab7eb46',
 		cover: direStraits,
 		interpret: 'Dire Straits',
 		song: 'Sultans of Swing',
-		lyrics: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et',
-		musicsheet: 'https://musescore.com/user/2538606/scores/4455576',
+		lyrics: 'https://genius.com/Dire-straits-sultans-of-swing-lyrics',
+		musicsheet: 'https://musescore.com/user/7788266/scores/3199221',
 	},
 	{
-		id: nanoid(),
+		id: '2aa01ee3-6ea4-4c74-832c-91dbbe5ed333',
 		cover: frankiVallie,
 		interpret: 'Frankie Vallie',
 		song: 'December 1963',
-		lyrics: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et',
-		musicsheet: 'https://musescore.com/user/2538606/scores/4455576',
+		lyrics: 'https://genius.com/The-four-seasons-december-1963-oh-what-a-night-lyrics',
+		musicsheet: 'https://musescore.com/garrettw/december-1963',
 	},
 	{
-		id: nanoid(),
+		id: 'db8c019c-2668-4198-8b53-727812124f47',
 		cover: ludovicoEinaudi,
 		interpret: 'Ludovico Einaudi',
-		song: 'Islands',
-		lyrics: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et',
-		musicsheet: 'https://musescore.com/user/2538606/scores/4455576',
+		song: 'I Giorni',
+		lyrics: 'There are no lyrics for this song',
+		musicsheet: 'https://www.stretta-music.de/einaudi-islands-essential-einaudi-nr-587028.html',
 	},
 ];
 export {inspirations};

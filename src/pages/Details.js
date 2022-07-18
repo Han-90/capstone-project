5a6a182b49e6fe8a5ddd6b8a55044cfe5eeaddc5
Gökhan Özdemir <goekhan.oezdemir90@googlemail.com>
2022-07-18 15:43:16 +0200

    style detailed page and cards

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,13 +1,26 @@
+import {Button} from '@mui/material';
 import {useParams} from 'react-router-dom';
-
+import styled from 'styled-components';
 export default function Details({inspirations}) {
 	const {id} = useParams();
 	const thisInspiration = inspirations.find(inspiration => inspiration.id === id);
 	return (
 		<>
 			<h2>{thisInspiration.interpret}</h2>
-			<p>{thisInspiration.lyrics}</p>
-			<img src={thisInspiration.cover} alt="Inspirationcards Images" />
+			<p>{thisInspiration.song}</p>
+			<Button variant="contained">
+				<a href={thisInspiration.lyrics}>Lyrics</a>
+			</Button>
+			<Button variant="contained">
+				<a href={thisInspiration.musicsheet}>Musicsheet</a>
+			</Button>
+			<br />
+			<img
+				src={thisInspiration.cover}
+				width="400px"
+				height="400px"
+				alt="Inspirationcards Images"
+			/>
 		</>
 	);
 }
